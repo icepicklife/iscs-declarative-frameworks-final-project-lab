90f82e3ece214efc602e2408f637caa4627adf16
icepicklife <161447128+icepicklife@users.noreply.github.com>
2025-12-01 11:49:31 +0800

    updated fixes on MyORM

--- GhettoORMProjectTemplate/src/orm/MyORM.java
+++ GhettoORMProjectTemplate/src/orm/MyORM.java
@@ -1,11 +1,17 @@
 package orm;
 
+import java.lang.reflect.*; 
 import java.util.HashMap;
+
+import io.github.lukehutch.fastclasspathscanner.*;
+
+import annotations.*;
+import dao.BasicMapper;
 
 public class MyORM 
 {	
 	
-	HashMap<Class, Class> entityToMapperMap = new HashMap<Class, Class>();
+	HashMap<Class<?>, Class<?>> entityToMapperMap = new HashMap<>();
 	
 	
 	public void init() throws Exception
@@ -28,6 +34,22 @@ public class MyORM
 		// check if the clazz has the @Entity annotation
 			// if not throw new RuntimeException("No @Entity")
 		// map the clazz to the mapper class
+		
+		new FastClasspathScanner("dao")
+			.matchClassesWithAnnotation(MappedClass.class, c -> {
+				
+				MappedClass mc = c.getAnnotation(MappedClass.class);
+                Class<?> entityClass = mc.clazz();
+
+                if (!entityClass.isAnnotationPresent(Entity.class)) {
+                    throw new RuntimeException("Entity class " + entityClass.getName() + " missing @Entity");
+                }
+
+                entityToMapperMap.put(entityClass, c);
+                System.out.println("ORM: Mapped " + entityClass.getSimpleName() + " -> " + c.getSimpleName());
+                
+			})
+			.scan();
 
 	}
 	
@@ -39,6 +61,32 @@ public class MyORM
 			// check if there is only 1 field with a Column id attribute
 			// if more than one field has id throw new RuntimeException("duplicate id=true")
 		
+		new FastClasspathScanner("entity")
+		.matchClassesWithAnnotation(MappedClass.class, c -> {
+			
+			int id_count = 0;
+			
+			for (Field fielders : c.getDeclaredFields()) {
+				
+				if (fielders.isAnnotationPresent(Column.class)) {
+					
+					Column retrieved_column = fielders.getAnnotation(Column.class);
+					
+					if (retrieved_column.id()) {
+						
+						id_count++;
+					}	
+				}
+			}
+			if (id_count != 1) {
+				
+				throw new RuntimeException("Entity " + c.getSimpleName() + "must have exactly 1 ID Column.");
+				
+			}  
+		})
+		.scan();
+		
+		
 		
 	}
 	
@@ -47,17 +95,43 @@ public class MyORM
 	{
 		// create the proxy object for the mapper class supplied in clazz parameter
 		// all proxies will use the supplied DaoInvocationHandler as the InvocationHandler
+		
+		Class<?> classMapper_interf = entityToMapperMap.get(clazz);
+		
+		if (classMapper_interf == null) {
+			
+			throw new RuntimeException("No Mapper interface found for entity: " + clazz.getSimpleName() + ".");
+		}
+		
+		DaoInvocationHandler project_handler = new DaoInvocationHandler();
+		
+		Object project_proxy = Proxy.newProxyInstance(
+								classMapper_interf.getClassLoader(),
+								new Class<?>[] { classMapper_interf },
+								project_handler);
 
-		return null;
+		return project_proxy;
 	}
 	
 
 	private void createTables()
 	{
 		// go through all the Mapper classes in the map
-			// create a proxy instance for each
-			// all these proxies can be casted to BasicMapper
-			// run the createTable() method on each of the proxies
+					// create a proxy instance for each
+					// all these proxies can be casted to BasicMapper
+					// run the createTable() method on each of the proxies
+		
+		for (Class<?> entityClazzes : entityToMapperMap.keySet()) {
+			
+			Object table_proxy = getMapper(entityClazzes);
+			BasicMapper<?> table_mappers = (BasicMapper<?>) table_proxy;
+			
+			table_mappers.createTable();
+			
+			System.out.println("Table created for: " + entityClazzes.getSimpleName());
+			
+		}
+		
 		
 	}
 
